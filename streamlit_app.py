2b39e4883752d2c4ec4d762413d5ca5440784526
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-10-04 15:02:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
+from langchain.llms import OpenAI
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title("Customer simulator")
 
-st.write('Hello world!')
+openai_api_key = st.secrets["openai"]
+
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
