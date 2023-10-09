fe804ef7b651bfe00671b04f683dd0a45eaeae9e
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-10-09 21:53:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 from langchain.schema import (
     AIMessage,
     HumanMessage,
@@ -14,6 +15,7 @@ def init():
     openai_api_key = st.secrets["openai"]
     chat=ChatOpenAI(temperature=0.5,openai_api_key=openai_api_key)
     customer_persona="You are a customer responding to a call from a sales person"
+    discussion=""
 
 def main():
     init()
@@ -40,8 +42,6 @@ def main():
          SystemMessage(content=customer_persona)
         ]
     
-    discussion=""
-    
     if prompt := st.chat_input("Start your call with an introduction"):
       st.session_state.messages.append(HumanMessage(content=prompt))
       with st.spinner ("Thinking..."):
@@ -51,11 +51,31 @@ def main():
     messages=st.session_state.get('messages',[])
     for i,msg in enumerate(messages[1:]): 
         if i % 2 == 0:
-        #    st.write('yes')
             message(msg.content,is_user=True,key=str(i)+'_saleperson')
+            discussion+=f"Sale person: {msg.content}. "
         else:
             message(msg.content,is_user=False,key=str(i)+'_customer')
+            discussion+=f"Customer: {msg.content}. "
+
+    st.write (discussion)
     
+def evaluate():
+    if discussion=="":
+        st.write("No discussion to evaluate")
+    else:
+        llm=OpenAI(temperature=0,openai_api_key=openai_api_key)
+        
+        #context of the discussion
+        context= "Evaluate this discussion between a sales person and a customer." 
+        context+="evaluate if a sales person arguments for a sell are convincing or not."
+        context+="please evaluate as regard following factors: "
+        context+="Clarity and Relevance,Credibility,Benefits and Value,Objection Handling,Emotional Appeal,Call to Action,Listening Skills,Comparison and Differentiation"
+        context+="Please provide a rating over 10 for the sales person per factor and a global rating. Below the discussion: "
+
+        
+    
+    
+
 main()
 
 
