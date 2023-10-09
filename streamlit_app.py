855cb763331a7e1d5f45517c082b489cb49ea1c3
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-10-09 18:15:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,10 @@
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import
 import streamlit as st
+from streamlit_chat import message
 
 st.title("Customer simulator")
 
 openai_api_key = st.secrets["openai"]
-chat = ChatOpenAI(openai_api_key=openai_api_key)
 
-with st.form("input form"):
-    st.write("<h3>Enter the customer personae ✨</h3>", unsafe_allow_html=True)
-    mode=st.selectbox('What mode do you want for customer?',('Receiving a call from a sales person', 'Doing a call to customer service'))
-    industry=st.text_input("Enter customer industry:")
-    position=st.text_input("Enter customer position:")
-    company_char=st.text_input("Enter company characterstics:")
-    pain_points=st.text_input("Enter pain points:")
-    decision_making_factors=st.text_input("Enter some decision factors:")
-    personnality=st.text_input("Enter some key personnality characteristics:")
-    customer_persona = f"You are a customer {mode}. you have the following characterstics."
-    customer_persona += f"You are in the industry of {industry}, you have the position of {position}."
-    customer_persona += f"The main characteristics of the company you are working for are {company_char}."
-    customer_persona += f"The main pain points in your business are {pain_points} and your decision making factors are {decision_making_factors}." 
-    customer_persona += f"your main personality trait are {personnality}."
-    customer_persona += f"you respond briefly to the question. you are a customer not an assistant "
-
-    if st.form_submit_button("Initiate discussion"):
-        if customer_persona is not None:
-            messages = [SystemMessage(content=customer_persona)]
-
-if prompt := st.chat_input("Start your call with an introduction"):
-    messages.append(HumanMessage(content=prompt))
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-
-    
-
-messages = [
-    SystemMessage(content="You are a helpful assistant that translates English to French."),
-    HumanMessage(content="I love programming.")
-]
 
