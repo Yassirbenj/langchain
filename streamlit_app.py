b0b17988ade02662243191aa911bb5dc0a66184a
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-10-09 18:32:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,27 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
 import streamlit as st
 from streamlit_chat import message
 
 st.title("Customer simulator")
 
 openai_api_key = st.secrets["openai"]
+
+chat=ChatOpenAI(temperature=0.5)
+
+messages=[
+ SystemMessage(content="you are a helpful assistant")
+]
+
+if prompt := st.chat_input("Start your call with an introduction"):
+  message(prompt,is_user=True)
+  messages.append(HumanMessage(content=prompt))
+  response=chat(message)
+  message(response.content,is_user=False)
 
 
