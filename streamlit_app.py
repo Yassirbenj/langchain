5510b235ca979bbfec7f2f0741b254f79150cc4a
Yassir Benjilali <116286449+Yassirbenj@users.noreply.github.com>
2023-10-09 21:21:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,27 @@ openai_api_key = st.secrets["openai"]
 
 chat=ChatOpenAI(temperature=0.5,openai_api_key=openai_api_key)
 
+with st.sidebar:
+    with st.form("input form"):
+        st.write("<h3>Enter the customer personae ✨</h3>", unsafe_allow_html=True)
+        mode=st.selectbox('What mode do you want for customer?',('Receiving a call from a sales person', 'Doing a call to customer service'))
+        industry=st.text_input("Enter customer industry:")
+        position=st.text_input("Enter customer position:")
+        company_char=st.text_input("Enter company characterstics:")
+        pain_points=st.text_input("Enter pain points:")
+        decision_making_factors=st.text_input("Enter some decision factors:")
+        personnality=st.text_input("Enter some key personnality characteristics:")
+        if st.form_submit_button("Initiate discussion"):
+            customer_persona = f"You are a customer {mode}. you have the following characterstics."
+            customer_persona += f"You are in the industry of {industry}, you have the position of {position}."
+            customer_persona += f"The main characteristics of the company you are working for are {company_char}."
+            customer_persona += f"The main pain points in your business are {pain_points} and your decision making factors are {decision_making_factors}." 
+            customer_persona += f"your main personality trait are {personnality}."
+            customer_persona += f"you respond briefly to the question. you are a customer not an assistant "
+
 if "messages" not in st.session_state:
     st.session_state.messages=[
-     SystemMessage(content="you are a helpful assistant")
+     SystemMessage(content=customer_persona)
     ]
 
 if prompt := st.chat_input("Start your call with an introduction"):
